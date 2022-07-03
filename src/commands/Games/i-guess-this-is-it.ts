e914fc49f96d9562c9e3d53c3bdb4abaed206d46
morbus <morbus@disobey.com>
2022-07-03 15:41:22 -0400

    Starting MessageEmbed proof-of-concept.

--- src/commands/Games/i-guess-this-is-it.ts
+++ src/commands/Games/i-guess-this-is-it.ts
@@ -1,8 +1,9 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command, CommandOptions } from '@sapphire/framework';
+import { MessageEmbed } from 'discord.js';
 import { send } from '@sapphire/plugin-editable-commands';
 import type { Args } from '@sapphire/framework';
-import type { Message } from 'discord.js';
+import type { GuildMember, Message } from 'discord.js';
 import gameData from '../../game-data/i-guess-this-is-it.json';
 
 @ApplyOptions<CommandOptions>({
@@ -11,39 +12,45 @@ import gameData from '../../game-data/i-guess-this-is-it.json';
 	description: 'A two-player game about saying goodbye.'
 })
 export class IGuessThisIsItCommand extends Command {
+	// @BOTNAME IGuessThisIsIt [GAMEID] ACTION [PLAYERS] [OPTIONS]
 	public override async messageRun(message: Message, args: Args) {
-		this.container.logger.debug(message.content);
 		const gameId = await args.pick('number').catch(() => 0);
 		const action = await args.pick('string').catch(() => 'help');
+		const players = await args.repeat('member').catch(() => [message.member]);
 
 		switch (action) {
 			case 'generate': {
-				return this.generate(message, args);
+				return this.generate(message, players);
 			}
 
 			case 'start': {
-				return this.start(message, args, gameId);
+				return this.start(message, gameId);
 			}
 		}
 
-		return this.help(message, args);
+		return null;
 	}
 
-	public async help(message: Message, args: Args) {
-		this.container.logger.debug(args);
-		return send(message, 'inside help');
-	}
-
-	public async generate(message: Message, args: Args) {
+	// @BOTNAME IGuessThisIsIt generate
+	// @BOTNAME IGuessThisIsIt generate @PLAYER1
+	// @BOTNAME IGuessThisIsIt generate @PLAYER1 @PLAYER2 --with-setup
+	public async generate(message: Message, players: (GuildMember | null)[]) {
 		// const randomElement = array[Math.floor(Math.random() * array.length)];
-		this.container.logger.debug(gameData.public.relationships);
-		const players = await args.repeat('member').catch(() => message.author);
 		this.container.logger.debug(players);
-		return send(message, 'inside generate');
+		this.container.logger.debug(gameData.public.relationships);
+		//this.container.logger.debug(players);
+
+		const embed = new MessageEmbed()
+			.setColor('#ff0000')
+			.setTitle('title')
+			// .setAuthor('Land of Idle Demons', message.author.avatarURL())
+			.setThumbnail('https://cdn.shopify.com/s/files/1/0728/8287/products/IGTII-Cover_2_large.png')
+			.setDescription('aaa');
+
+		return message.reply({ embeds: [embed] });
 	}
 
-	public async start(message: Message, args: Args, gameId: number) {
-		this.container.logger.debug(args);
+	public async start(message: Message, gameId: number) {
 		this.container.logger.debug(gameId);
 		return send(message, 'inside start');
 	}
