92ad62bd79d48f09de77918d299d302edd31cfaa
morbus <morbus@disobey.com>
2022-07-04 12:39:11 -0400

    Move arrayShuffle() to lib/utils.js:shuffle().

--- src/commands/Games/i-guess-this-is-it.ts
+++ src/commands/Games/i-guess-this-is-it.ts
@@ -2,6 +2,7 @@ import { ApplyOptions } from '@sapphire/decorators';
 import { Command, CommandOptions } from '@sapphire/framework';
 import { MessageEmbed } from 'discord.js';
 import { send } from '@sapphire/plugin-editable-commands';
+import { shuffle } from '../../lib/utils';
 import gameData from '../../game-data/i-guess-this-is-it.json';
 import type { Args } from '@sapphire/framework';
 import type { GuildMember, Message } from 'discord.js';
@@ -36,11 +37,11 @@ export class IGuessThisIsItCommand extends Command {
 	// @BOTNAME IGuessThisIsIt generate @PLAYER1
 	// @BOTNAME IGuessThisIsIt generate @PLAYER1 @PLAYER2 --with-setup
 	public async generate(message: Message, players: (GuildMember | null)[]) {
-		const relationship = this.arrayShuffle(this.arrayShuffle(gameData.public.relationships).shift());
-		const reasonForSayingGoodbye = this.arrayShuffle(gameData.public.reasonsForSayingGoodbye).shift();
-		const location = this.arrayShuffle(gameData.public.locations).shift();
+		const relationship = shuffle(shuffle(gameData.public.relationships).shift());
+		const reasonForSayingGoodbye = shuffle(gameData.public.reasonsForSayingGoodbye).shift();
+		const location = shuffle(gameData.public.locations).shift();
 
-		players = this.arrayShuffle(players);
+		players = shuffle(players);
 		const playerLeaving = players[0] ? players[0].displayName : 'Player 1';
 		const playerStaying = players[1] ? players[1].displayName : 'Player 2';
 
@@ -50,7 +51,7 @@ export class IGuessThisIsItCommand extends Command {
 			.setThumbnail('https://github.com/morbus/button-shy-games-play-bot/raw/main/docs/assets/i-guess-this-is-it--cover.png')
 			.setDescription('@TODO HELP')
 			.addField(`${playerLeaving} roleplays as`, `a ${relationship.shift()} saying goodbye because ${reasonForSayingGoodbye}.`, true)
-			.addField(`${playerStaying} roleplays as`, `a ${relationship.shift()} who is staying.`, true)
+			.addField(`${playerStaying} roleplays as`, `a ${relationship.shift()}.`, true)
 			.addField('Location', location, true);
 		return message.reply({ embeds: [embed] });
 	}
@@ -58,22 +59,5 @@ export class IGuessThisIsItCommand extends Command {
 	public async start(message: Message, gameId: number) {
 		this.container.logger.debug(gameId);
 		return send(message, 'inside start');
-	}
-
-	/**
-	 * From https://github.com/sindresorhus/array-shuffle.
-	 * @todo Move this to a library thing and explain Error
-	 * when loading Error [ERR_REQUIRE_ESM]: require() of ES Module array-shuffle/index.js is not supported.
-	 * @param array
-	 */
-	public arrayShuffle(array: Array<any>) {
-		array = [...array];
-
-		for (let index = array.length - 1; index > 0; index--) {
-			const newIndex = Math.floor(Math.random() * (index + 1));
-			[array[index], array[newIndex]] = [array[newIndex], array[index]];
-		}
-
-		return array;
 	}
 }
---
+++ src/lib/utils.ts
@@ -0,0 +1,16 @@
+/**
+ * Shuffle an array using the Durstenfeld algorithm, based on Fisher–Yates.
+ *
+ * Copied from https://github.com/sindresorhus/array-shuffle so we needn't
+ * deal with "[ERR_REQUIRE_ESM]: require() of ES Module is not supported".
+ */
+export function shuffle(array: Array<any>) {
+	array = [...array];
+
+	for (let index = array.length - 1; index > 0; index--) {
+		const newIndex = Math.floor(Math.random() * (index + 1));
+		[array[index], array[newIndex]] = [array[newIndex], array[index]];
+	}
+
+	return array;
+}
