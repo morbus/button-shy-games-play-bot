431a41b7a18117df0f7a980b9fa7b3f5ac3a2842
morbus <morbus@disobey.com>
2022-07-04 11:19:58 -0400

    First attempt at actually working generator.

--- src/commands/Games/i-guess-this-is-it.ts
+++ src/commands/Games/i-guess-this-is-it.ts
@@ -2,9 +2,9 @@ import { ApplyOptions } from '@sapphire/decorators';
 import { Command, CommandOptions } from '@sapphire/framework';
 import { MessageEmbed } from 'discord.js';
 import { send } from '@sapphire/plugin-editable-commands';
+import gameData from '../../game-data/i-guess-this-is-it.json';
 import type { Args } from '@sapphire/framework';
 import type { GuildMember, Message } from 'discord.js';
-import gameData from '../../game-data/i-guess-this-is-it.json';
 
 @ApplyOptions<CommandOptions>({
 	name: 'IGuessThisIsIt',
@@ -14,6 +14,7 @@ import gameData from '../../game-data/i-guess-this-is-it.json';
 export class IGuessThisIsItCommand extends Command {
 	// @BOTNAME IGuessThisIsIt [GAMEID] ACTION [PLAYERS] [OPTIONS]
 	public override async messageRun(message: Message, args: Args) {
+		this.container.logger.debug(message.content);
 		const gameId = await args.pick('number').catch(() => 0);
 		const action = await args.pick('string').catch(() => 'help');
 		const players = await args.repeat('member').catch(() => [message.member]);
@@ -35,25 +36,46 @@ export class IGuessThisIsItCommand extends Command {
 	// @BOTNAME IGuessThisIsIt generate @PLAYER1
 	// @BOTNAME IGuessThisIsIt generate @PLAYER1 @PLAYER2 --with-setup
 	public async generate(message: Message, players: (GuildMember | null)[]) {
-		// const randomElement = array[Math.floor(Math.random() * array.length)];
-		this.container.logger.debug(players);
-		this.container.logger.debug(gameData.public.relationships);
-		this.container.logger.debug(message.content);
-		//this.container.logger.debug(players);
+		const relationship = this.arrayShuffle(this.arrayShuffle(gameData.public.relationships).shift());
+		const reasonForSayingGoodbye = this.arrayShuffle(gameData.public.reasonsForSayingGoodbye).shift();
+		const location = this.arrayShuffle(gameData.public.locations).shift();
+
+		this.container.logger.debug(players[1]);
+
+		players = this.arrayShuffle(players);
+		const playerLeaving = players[0] ? players[0].displayName : 'Player 1';
+		const playerStaying = players[1] ? players[1].displayName : 'Player 2';
 
 		const embed = new MessageEmbed()
-			.setColor('#ff0000')
-			.setTitle('I Guess This Is It #1231')
+			.setColor('#d8d2cd')
+			.setTitle('#@TODO: I Guess This Is It')
 			.setThumbnail('https://github.com/morbus/button-shy-games-play-bot/raw/main/docs/assets/i-guess-this-is-it--cover.png')
-			.setDescription('Game ID #1231')
-			.addField('Player 1', "roleplays as a soldier who is saying goodbye because they're going to space.", true)
-			.addField('@MorbusIff', 'roleplays as a soldier who is staying.', true)
-			.addField('Location', 'a school yard');
+			.setDescription('FURTHER INSTRUCTIONS GO HERE SO THAT PLAYERS KNOW WHAT TO DO NEXT')
+			.addField(`${playerLeaving} roleplays as`, `a ${relationship.shift()} who is saying goodbye because ${reasonForSayingGoodbye}.`, true)
+			.addField(`${playerStaying} roleplays as`, `a ${relationship.shift()} who is staying.`, true)
+			.addField('Location', location, true);
 		return message.reply({ embeds: [embed] });
 	}
 
 	public async start(message: Message, gameId: number) {
 		this.container.logger.debug(gameId);
 		return send(message, 'inside start');
+	}
+
+	/**
+	 * From https://github.com/sindresorhus/array-shuffle.
+	 * @todo Move this to a library thing and explain Error
+	 * when loading Error [ERR_REQUIRE_ESM]: require() of ES Module array-shuffle/index.js is not supported.
+	 * @param array
+	 */
+	public arrayShuffle(array: Array<any>) {
+		array = [...array];
+
+		for (let index = array.length - 1; index > 0; index--) {
+			const newIndex = Math.floor(Math.random() * (index + 1));
+			[array[index], array[newIndex]] = [array[newIndex], array[index]];
+		}
+
+		return array;
 	}
 }
